03ce558ca5a2d8188655ee77acb73ca253786a7f
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2025-06-05 14:49:38 +0800

    add berita activity. displays news details

---
+++ app/src/main/java/com/abcd/aksan_aplikasipelayanankantordesa/ui/activity/user/berita/BeritaActivity.kt
@@ -0,0 +1,69 @@
+package com.abcd.aksan_aplikasipelayanankantordesa.ui.activity.user.berita
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.abcd.aksan_aplikasipelayanankantordesa.R
+import com.abcd.aksan_aplikasipelayanankantordesa.data.model.BeritaModel
+import com.abcd.aksan_aplikasipelayanankantordesa.databinding.ActivityBeritaBinding
+import com.abcd.aksan_aplikasipelayanankantordesa.utils.Constant
+import com.abcd.aksan_aplikasipelayanankantordesa.utils.TanggalDanWaktu
+import com.bumptech.glide.Glide
+
+class BeritaActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBeritaBinding
+    private lateinit var berita : BeritaModel
+    private var tanggalDanWaktu = TanggalDanWaktu()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBeritaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setNavTopBar()
+        setDataSebelumnya()
+    }
+
+    private fun setNavTopBar() {
+        binding.navTopBar.apply {
+            tvTitle.text = "Detail Berita"
+            ivNavDrawer.visibility = View.GONE
+            ivBack.visibility = View.VISIBLE
+
+            ivBack.setOnClickListener {
+                finish()
+            }
+        }
+    }
+
+    private fun setDataSebelumnya() {
+        val bundle = intent.extras
+        if(bundle!=null){
+            berita = bundle.getParcelable("berita")!!
+            setBerita(berita)
+        }
+    }
+
+    private fun setBerita(berita: BeritaModel) {
+        binding.apply {
+            try {
+                tvJudul.text = berita.judul
+                tvKelurahan.text = "Kel. ${berita.kelurahan!!.kelurahan}"
+                tvTanggal.text = "|  ${tanggalDanWaktu.konversiBulanSingkatan(berita.tanggal!!)}"
+                tvBerita.text = berita.isi
+
+                Glide.with(this@BeritaActivity)
+                    .load("${Constant.LOCATION_GAMBAR_BERITA}${berita.gambar}") // URL Gambar
+                    .placeholder(R.drawable.loading)
+                    .error(R.drawable.ic_berita_error)
+                    .into(ivBerita) // imageView mana yang akan diterapkan
+
+            } catch(ex: Exception){
+                Toast.makeText(this@BeritaActivity, ex.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
