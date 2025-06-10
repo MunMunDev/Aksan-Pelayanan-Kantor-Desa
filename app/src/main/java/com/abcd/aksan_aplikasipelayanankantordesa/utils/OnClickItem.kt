1086e070387437e1d032acba6c3e318177261161
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2025-06-10 22:50:00 +0800

    create an adapter to display document data in recyclerView

--- app/src/main/java/com/abcd/aksan_aplikasipelayanankantordesa/utils/OnClickItem.kt
+++ app/src/main/java/com/abcd/aksan_aplikasipelayanankantordesa/utils/OnClickItem.kt
@@ -3,6 +3,7 @@ package com.abcd.aksan_aplikasipelayanankantordesa.utils
 import android.view.View
 import com.abcd.aksan_aplikasipelayanankantordesa.data.model.BeritaModel
 import com.abcd.aksan_aplikasipelayanankantordesa.data.model.BerkasModel
+import com.abcd.aksan_aplikasipelayanankantordesa.data.model.DokumenModel
 import com.abcd.aksan_aplikasipelayanankantordesa.data.model.UserModel
 
 interface OnClickItem {
@@ -18,23 +19,12 @@ interface OnClickItem {
         )
     }
 
-    interface ClickRiwayatPesanan{
-        fun clickRiwayatPesanan(
-            idPemesanan: Int
+    interface ClickDokumen{
+        fun clickGambarDokumen(
+            dokumen: DokumenModel
+        )
+        fun clickUploadDokumen(
+            dokumen: DokumenModel
         )
     }
-
-    interface ClickAkun{
-        fun clickItemSetting(akun: UserModel, it: View)
-        fun clickItemAlamat(alamat: String, it: View)
-    }
-
-
-    interface ClickAdminUser{
-        fun clickItemNama(title:String, nama: String)
-        fun clickItemAlamat(title:String, alamat: String)
-        fun clickItemUsername(title:String, username: String)
-        fun clickItemSetting(wo: UserModel, it: View)
-    }
-
 }
