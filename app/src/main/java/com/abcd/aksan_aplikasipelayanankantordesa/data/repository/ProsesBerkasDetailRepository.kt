236599c98c37b23661126c19b987dd655c3584cc
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2025-08-07 21:31:26 +0800

    menambahkan upload berkas ketika dokumen yang dimasukkan user tidak sesuai dengan dokumen yang dibutuhkan desa.

--- app/src/main/java/com/abcd/aksan_aplikasipelayanankantordesa/data/repository/ProsesBerkasDetailRepository.kt
+++ app/src/main/java/com/abcd/aksan_aplikasipelayanankantordesa/data/repository/ProsesBerkasDetailRepository.kt
@@ -1,9 +1,10 @@
 package com.abcd.aksan_aplikasipelayanankantordesa.data.repository
 
 import com.abcd.aksan_aplikasipelayanankantordesa.data.database.api.ApiService
-import com.abcd.aksan_aplikasipelayanankantordesa.data.model.BerkasModel
 import com.abcd.aksan_aplikasipelayanankantordesa.data.model.DokumenModel
 import com.abcd.aksan_aplikasipelayanankantordesa.data.model.ResponseModel
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -15,6 +16,18 @@ class ProsesBerkasDetailRepository @Inject constructor(
         idBerkas:Int
     ): ArrayList<DokumenModel>{
         val data = api.getAllProsesBerkasDetail("", idBerkas)
+        return data
+    }
+
+    suspend fun postUploadBerkas(
+        post: RequestBody,
+        idDokumen: RequestBody,
+        idBerkas: RequestBody,
+        noKtp: RequestBody,
+        dokumen: RequestBody,
+        file: MultipartBody.Part
+    ): ResponseModel{
+        val data = api.postUploadDokumen(post, idDokumen, idBerkas, noKtp, dokumen, file)
         return data
     }
 }
--- app/src/main/java/com/abcd/aksan_aplikasipelayanankantordesa/ui/activity/user/proses/detail/ProsesBerkasDetailActivity.kt
+++ app/src/main/java/com/abcd/aksan_aplikasipelayanankantordesa/ui/activity/user/proses/detail/ProsesBerkasDetailActivity.kt
@@ -1,23 +1,33 @@
 package com.abcd.aksan_aplikasipelayanankantordesa.ui.activity.user.proses.detail
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.OpenableColumns
+import android.provider.Settings
 import android.util.Log
 import android.view.View
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.abcd.aksan_aplikasipelayanankantordesa.R
 import com.abcd.aksan_aplikasipelayanankantordesa.adapter.DokumenAdapter
 import com.abcd.aksan_aplikasipelayanankantordesa.data.model.BerkasModel
 import com.abcd.aksan_aplikasipelayanankantordesa.data.model.DokumenModel
+import com.abcd.aksan_aplikasipelayanankantordesa.data.model.ResponseModel
 import com.abcd.aksan_aplikasipelayanankantordesa.databinding.ActivityProsesBerkasDetailBinding
 import com.abcd.aksan_aplikasipelayanankantordesa.databinding.AlertDialogImageBinding
+import com.abcd.aksan_aplikasipelayanankantordesa.databinding.AlertDialogUploadDokumenBinding
 import com.abcd.aksan_aplikasipelayanankantordesa.ui.activity.user.pdf.PdfActivity
 import com.abcd.aksan_aplikasipelayanankantordesa.utils.Constant
 import com.abcd.aksan_aplikasipelayanankantordesa.utils.LoadingAlertDialog
@@ -26,8 +36,12 @@ import com.abcd.aksan_aplikasipelayanankantordesa.utils.SharedPreferencesLogin
 import com.abcd.aksan_aplikasipelayanankantordesa.utils.network.UIState
 import com.bumptech.glide.Glide
 import dagger.hilt.android.AndroidEntryPoint
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.net.URLConnection
 import java.util.ArrayList
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class ProsesBerkasDetailActivity : AppCompatActivity() {
@@ -35,16 +49,30 @@ class ProsesBerkasDetailActivity : AppCompatActivity() {
     private val viewModel: ProsesBerkasDetailViewModel by viewModels()
     private lateinit var dokumenAdapter : DokumenAdapter
     private var berkas: BerkasModel? = null
+    private var idBerkas: Int = 0
     private lateinit var sharedPreferences: SharedPreferencesLogin
+    private var file: MultipartBody.Part? = null
+    private var tempView: AlertDialogUploadDokumenBinding? = null
+    private var loading = LoadingAlertDialog()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProsesBerkasDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         sharedPreferences = SharedPreferencesLogin(this@ProsesBerkasDetailActivity)
+        permissionDocument()
         retrievePreviousData()
         setNavTopBar()
         getDokumen()
+        getUpdateDokumen()
+    }
+
+    private fun permissionDocument() {
+        if(checkPermission()){
+//            Toast.makeText(this@ProsesBerkasDetailActivity, "Granted", Toast.LENGTH_SHORT).show()
+        } else{
+            requestPermission()
+        }
     }
 
     @Suppress("DEPRECATION")
@@ -52,7 +80,8 @@ class ProsesBerkasDetailActivity : AppCompatActivity() {
         val bundle = intent.extras
         if(bundle != null){
             berkas = bundle.getParcelable("berkas")
-            fetchDokumen(berkas?.id_berkas!!)
+            idBerkas = berkas?.id_berkas!!
+            fetchDokumen(idBerkas)
         }
     }
 
@@ -114,13 +143,94 @@ class ProsesBerkasDetailActivity : AppCompatActivity() {
                 }
 
                 override fun clickUploadDokumen(dokumen: DokumenModel) {
-
+                    setShowUploadDokumen(dokumen)
                 }
             }, berkas?.id_berkas!!)
             rvDokumen.apply {
-                layoutManager = GridLayoutManager(this@ProsesBerkasDetailActivity, 2)
+                layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
                 adapter = dokumenAdapter
             }
+        }
+    }
+
+    private fun setShowUploadDokumen(dokumen: DokumenModel) {
+        val view = AlertDialogUploadDokumenBinding.inflate(layoutInflater)
+
+        val alertDialog = AlertDialog.Builder(this@ProsesBerkasDetailActivity)
+        alertDialog.setView(view.root)
+            .setCancelable(false)
+        val dialogInputan = alertDialog.create()
+        dialogInputan.show()
+
+        tempView = view
+        view.apply {
+            tvTitle.text = dokumen.dokumen
+            if(dokumen.pesan_desa!!.isNotEmpty()) tvPesanDesa.text = dokumen.pesan_desa
+            tvDokumen.setOnClickListener {
+                if(checkPermission()){
+                    pickFile()
+                }
+            }
+            btnSimpan.setOnClickListener {
+                var cek = false
+                if (tvDokumen.text.toString() == resources.getString(R.string.upload_dokumen)) {
+                    tvDokumen.error = "Tidak Boleh Kosong"
+                    cek = true
+                }
+                if(!cek){
+                    val post = convertStringToMultipartBody("post_upload_dokumen")
+                    val idDokumen = convertStringToMultipartBody(dokumen.id_dokumen.toString())
+                    val idBerkas = convertStringToMultipartBody(dokumen.id_berkas.toString())
+                    val noKtp = convertStringToMultipartBody(sharedPreferences.getNoKtp())
+                    val tDokumen = convertStringToMultipartBody(dokumen.dokumen!!)
+
+                    Log.d("ProsesBerkasTAG", "setShowUploadDokumen: ${dokumen.id_dokumen.toString()}")
+                    Log.d("ProsesBerkasTAG", "setShowUploadDokumen: ${dokumen.id_berkas.toString()}")
+                    Log.d("ProsesBerkasTAG", "setShowUploadDokumen: ${sharedPreferences.getNoKtp()}")
+
+                    postUpdateDokumen(post, idDokumen, idBerkas, noKtp, tDokumen, file!!)
+                    dialogInputan.dismiss()
+                }
+            }
+            btnBatal.setOnClickListener {
+                dialogInputan.dismiss()
+            }
+        }
+    }
+
+    private fun postUpdateDokumen(
+        post: RequestBody,
+        idDokumen: RequestBody,
+        idBerkas: RequestBody,
+        noKtp: RequestBody,
+        dokumen: RequestBody,
+        file: MultipartBody.Part
+    ) {
+        viewModel.postUploadDokumen(post, idDokumen, idBerkas, noKtp, dokumen, file)
+    }
+
+    private fun getUpdateDokumen(){
+        viewModel.getUploadDokumen.observe(this@ProsesBerkasDetailActivity){result->
+            when(result){
+                is UIState.Loading-> loading.alertDialogLoading(this@ProsesBerkasDetailActivity)
+                is UIState.Success-> setSuccessUpdateDokumen(result.data)
+                is UIState.Failure-> setFailureUpdateDokumen(result.message)
+            }
+        }
+    }
+
+    private fun setFailureUpdateDokumen(message: String) {
+        Toast.makeText(this@ProsesBerkasDetailActivity, message, Toast.LENGTH_SHORT).show()
+        loading.alertDialogCancel()
+    }
+
+    private fun setSuccessUpdateDokumen(data: ResponseModel) {
+        loading.alertDialogCancel()
+        if(data.status == "0"){
+            Toast.makeText(this@ProsesBerkasDetailActivity, "Berhasil. Silahkan Menunggu Verifikasi Dari Desa", Toast.LENGTH_SHORT).show()
+            fetchDokumen(idBerkas)
+        } else{
+            Toast.makeText(this@ProsesBerkasDetailActivity, data.message_response, Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -162,4 +272,101 @@ class ProsesBerkasDetailActivity : AppCompatActivity() {
             rvDokumen.visibility = View.VISIBLE
         }
     }
+
+    //Permission
+    private fun checkPermission(): Boolean{
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            //Android is 11(R) or above
+            Environment.isExternalStorageManager()
+        }
+        else{
+            //Android is below 11(R)
+            val write = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            val read = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+            write == PackageManager.PERMISSION_GRANTED && read == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    private fun getNameFile(uri: Uri): String {
+        val cursor = contentResolver.query(uri, null, null, null, null)
+        val nameIndex = cursor?.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+        cursor?.moveToFirst()
+        val name = cursor?.getString(nameIndex!!)
+        cursor?.close()
+        return name!!
+    }
+
+    @SuppressLint("Recycle")
+    private fun uploadImageToStorage(pdfUri: Uri?, pdfFileName: String): MultipartBody.Part? {
+        var pdfPart : MultipartBody.Part? = null
+        pdfUri?.let {
+            val file = contentResolver.openInputStream(pdfUri)?.readBytes()
+
+            if (file != null) {
+                pdfPart = convertFileToMultipartBody(file, pdfFileName)
+            }
+        }
+        return pdfPart
+    }
+
+    private fun convertFileToMultipartBody(file: ByteArray, fileName: String): MultipartBody.Part {
+        // Tentukan MIME type dari ekstensi nama file
+        val mimeType = URLConnection.guessContentTypeFromName(fileName) ?: "application/octet-stream"
+
+        // Buat RequestBody dari ByteArray
+        val requestFile = file.toRequestBody(mimeType.toMediaTypeOrNull())
+
+        // Bungkus ke dalam MultipartBody.Part
+        return MultipartBody.Part.createFormData("file", fileName, requestFile)
+    }
+
+    private fun convertStringToMultipartBody(data: String): RequestBody {
+        return RequestBody.create("multipart/form-data".toMediaTypeOrNull(), data)
+    }
+
+    private fun requestPermission(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            if (Environment.isExternalStorageManager()) {
+                startActivity(Intent(this@ProsesBerkasDetailActivity, ProsesBerkasDetailActivity::class.java))
+            } else { //request for the permission
+                val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+                val uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            }
+        } else{
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE),
+                Constant.STORAGE_PERMISSION_CODE
+            )
+        }
+    }
+
+    private fun pickFile() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            type = "*/*"
+            addCategory(Intent.CATEGORY_OPENABLE)
+            putExtra(Intent.EXTRA_MIME_TYPES, arrayOf("image/*", "application/pdf"))
+        }
+        startActivityForResult(intent, Constant.STORAGE_PERMISSION_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == Constant.STORAGE_PERMISSION_CODE && resultCode == Activity.RESULT_OK && data != null) {
+            // Mendapatkan URI file PDF yang dipilih
+            val fileUri = data.data!!
+
+            val nameImage = getNameFile(fileUri)
+
+            tempView?.let {
+                it.tvDokumen.text = nameImage
+            }
+
+            // Mengirim file PDF ke website menggunakan Retrofit
+            file = uploadImageToStorage(fileUri, nameImage)
+        }
+    }
+
 }
